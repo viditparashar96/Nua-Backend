fe4d05595ca1c2bdf88708bf2d8574b11e8eb557
vidit <viditsharma818@gmail.com>
2024-05-22 19:04:35 +0530

    added cors origin

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(logger("dev"));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://nua-eight.vercel.app/login"],
     credentials: true,
   })
 );
