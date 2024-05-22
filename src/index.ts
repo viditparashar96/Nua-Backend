f7f35a35ce1fcb99733a778ffac2e091790911a0
vidit <viditsharma818@gmail.com>
2024-05-22 19:12:30 +0530

    cors fixed

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(logger("dev"));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://nua-eight.vercel.app/login"],
+    origin: ["http://localhost:5173", "https://nua-eight.vercel.app"],
     credentials: true,
   })
 );
